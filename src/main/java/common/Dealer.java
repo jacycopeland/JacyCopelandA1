f2c05015377fcd60025803ead1b907a9b9917584
JacyCopeland <jacycopeland@gmail.com>
2018-09-16 19:47:49 -0400

    Added initial draw funition and scoring function for the dealer

--- src/main/java/common/Dealer.java
+++ src/main/java/common/Dealer.java
@@ -29,6 +29,32 @@ public class Dealer {
 		score = n;
 	}
 	
+//function that draws the initil hand
+	public void initilDraw(Deck d)
+	{
+		hand.add(d.drawCard());
+		hand.add(d.drawCard());
+		
+		System.out.println("The Dealer's hand is ? and " + hand.get(1));
+		
+		//show that the card is face up
+		hand.get(1).setFace();
+		
+	}
+	
+	//function for adding and checking the score
+	public int checkScore()
+	{
+		score = score + hand.get(hand.size()-1).getPoints();
+		
+		if(score < 11 && hand.get(size()-1).getNumber() == "A")
+		{
+			score = score +10;
+			
+			return score;
+		}
+	}
+	
 	
 
 }
